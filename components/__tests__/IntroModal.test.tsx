f9bda02ed64f4876bb0c55f09f0e50e042e82dec
raggnarD <james.kocher@gmail.com>
2025-12-29 11:27:03 -0500

    test fixes

--- components/__tests__/IntroModal.test.tsx
+++ components/__tests__/IntroModal.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import IntroModal from '../IntroModal'
 
@@ -18,47 +18,41 @@ jest.mock('../NeighborhoodFinderIntro', () => {
 jest.mock('@/contexts/WizardContext', () => ({
   useWizard: () => ({
     wizardActive: false,
+    setWizardActive: jest.fn(),
     setWizardStep: jest.fn(),
   }),
 }))
 
-describe('IntroModal', () => {
-  const mockOnClose = jest.fn()
+// Mock localStorage
+const localStorageMock = {
+  getItem: jest.fn(() => null),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+}
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
+  writable: true,
+})
 
+describe('IntroModal', () => {
   beforeEach(() => {
     jest.clearAllMocks()
+    localStorageMock.getItem.mockReturnValue(null) // Default: hasn't seen intro
   })
 
-  it('should render when isOpen is true', async () => {
-    render(<IntroModal isOpen={true} onClose={mockOnClose} />)
+  it('should render when user has not seen intro', async () => {
+    localStorageMock.getItem.mockReturnValue(null)
+    render(<IntroModal />)
     await waitFor(() => {
       expect(screen.getByText(/welcome to rushroost/i)).toBeInTheDocument()
-    })
+    }, { timeout: 3000 })
   })
 
-  it('should not render when isOpen is false', () => {
-    const { container } = render(<IntroModal isOpen={false} onClose={mockOnClose} />)
+  it('should not render when user has seen intro', () => {
+    localStorageMock.getItem.mockReturnValue('true')
+    const { container } = render(<IntroModal />)
     expect(container.firstChild).toBeNull()
-  })
-
-  it('should call onClose when close button is clicked', async () => {
-    render(<IntroModal isOpen={true} onClose={mockOnClose} />)
-    await waitFor(() => {
-      const closeButton = screen.getByRole('button', { name: /close/i })
-      fireEvent.click(closeButton)
-    })
-    expect(mockOnClose).toHaveBeenCalledTimes(1)
-  })
-
-  it('should show NeighborhoodFinderIntro when Get Started is clicked', async () => {
-    render(<IntroModal isOpen={true} onClose={mockOnClose} />)
-    await waitFor(() => {
-      const getStartedButton = screen.getByRole('button', { name: /get started/i })
-      fireEvent.click(getStartedButton)
-    })
-    await waitFor(() => {
-      expect(screen.getByTestId('neighborhood-finder-intro')).toBeInTheDocument()
-    })
   })
 })
 
--- jest.setup.js
+++ jest.setup.js
@@ -1,11 +1,11 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+import React from 'react'
 import { act } from 'react'
 
-// Polyfill for React 19 act
-global.React = require('react')
-if (!global.React.act) {
-  global.React.act = act
+// Polyfill for React 19 act - ensure React.act is available
+if (!React.act) {
+  React.act = act
 }
 
 // Mock Next.js router
