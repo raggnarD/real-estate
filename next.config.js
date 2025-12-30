e087ea88c41f803e1395363826082614b677cd3e
raggnarD <james.kocher@gmail.com>
2025-12-30 13:01:18 -0500

    attempt to fix build error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // This ensures Next.js doesn't look for a lint directory
+    dirs: ['.'],
+  },
 }
 
 module.exports = nextConfig
