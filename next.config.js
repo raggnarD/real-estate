00c80879365d905b0a61a0a0b0bfff4d5b60ecfa
raggnarD <james.kocher@gmail.com>
2025-12-30 13:49:04 -0500

    test setup

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    // Disable ESLint during builds - we run it separately via npm run lint
-    ignoreDuringBuilds: true,
-  },
 }
 
 module.exports = nextConfig
